8742d17ccf2d929c6ff2549365755dd34c64dde7
IttaRodrigues <itamararodriguesdeoliveira@gmail.com>
2025-02-26 20:34:31 -0300

    Deixando dinamica

--- GStore/Models/Produto.cs
+++ GStore/Models/Produto.cs
@@ -25,7 +25,7 @@ namespace GStore.Models;
 
         [Required]
         [Range(0, int.MaxValue)]
-        public int Qtde { get; set; }
+        public int QtdeEstoque { get; set; }
 
         
         [Range(0, double.MaxValue)]
@@ -35,4 +35,8 @@ namespace GStore.Models;
         [Range(0, double.MaxValue)]
         [Column(TypeName = "numeric(10,2)")]      
         public decimal ValorVenda { get; set; }
+
+        public bool Destaque { get; set; } = false;
+
+        public List<ProdutoFoto> Fotos { get; set; }
     }
